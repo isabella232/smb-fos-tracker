e2e1aff365732192da5e333cc4feb0f55b1c76ac
vahinim <vahinim@google.com>
2020-06-09 16:47:17 +0530

    Add constant strings representing store verification status in Verification.java model

--- FosServer/src/main/java/com/example/fostracker/models/Verification.java
+++ FosServer/src/main/java/com/example/fostracker/models/Verification.java
@@ -14,18 +14,41 @@ public class Verification {
     Timestamp verificationCreationDateTime;
 
     /**
+     * If store verification is successful then database table stores "green" String.
+     * else if merchant is not registered then database table stores "yellow" String
+     * else if store verification is  unsuccessful then database table stores "red" String
+     */
+    private final String VERIFICATION_SUCCESSFUL = "green";
+    private final String NOT_REGISTERED = "yellow";
+    private final String VERIFICATION_UNSUCCESSFUL = "red";
+
+    /**
      * @param agentEmail - email id of the agent who completed the verification
      * @param storePhone - the store phone number that was verified
      * @param verificationCoordinates - the location the agent was at at the time of verification
      * @param verificationStatus - whether the verification was successful/unsuccessful/incomplete
      * verificationDateTime is set by default to the current time of Asia/Kolkata and cannot be changed later
      */
-    public Verification(String agentEmail, String storePhone, Coordinates verificationCoordinates, String verificationStatus) {
+    public Verification(String agentEmail, String storePhone, Coordinates verificationCoordinates, int verificationStatus) {
         this.agentEmail = agentEmail;
         this.storePhone = storePhone;
         this.verificationCoordinates = verificationCoordinates;
-        this.verificationStatus = verificationStatus;
+        this.verificationStatus = getStoreVerificationString(verificationStatus);
         this.verificationCreationDateTime = Timestamp.valueOf(ZonedDateTime.now(ZoneId.of("Asia/Kolkata")).toLocalDateTime());
+    }
+
+    //Based on the status integral value it sets to required status string
+    private String getStoreVerificationString(int verificationStatus){
+        switch(verificationStatus){
+            case 0:
+                return VERIFICATION_UNSUCCESSFUL;
+            case -1:
+                return NOT_REGISTERED;
+            case 1:
+                return VERIFICATION_SUCCESSFUL;
+            default:
+                return null;
+        }
     }
 
     public String getAgentEmail() {
