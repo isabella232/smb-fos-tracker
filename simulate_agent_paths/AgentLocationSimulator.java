9b777ad248b4ba3bb31bf9adaec738832726da4b
pragyasethi <pragyasethi@google.com>
2020-06-12 12:44:03 +0530

    Created toJson() function specific to class

--- simulate_agent_paths/AgentLocationSimulator.java
+++ simulate_agent_paths/AgentLocationSimulator.java
@@ -7,37 +7,64 @@ import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
 
 /**
+ * Class for holding details of agent that are required to change his/her location in
+ * spanner.
+ * @param AgentEmail is the email address with which agent is registered in database
+ * @param AgentLatitude is current latitude of agent's location
+ * @param AgentLongitude is current longitude of agent's location
+ */
+class AgentLocationInfo{
+    String AgentEmail;
+    double AgentLatitude;
+    double AgentLongitude;
+    String toJson(){
+        return "{"
+                + "\"email\":\"" + AgentEmail + "\","
+                + "\"latitude\":" + Double.toString(AgentLatitude) + ","
+                + "\"longitude\":" + Double.toString(AgentLongitude)
+                + "}";
+    }
+    AgentLocationInfo(String email, double latitude, double longitude){
+        this.AgentEmail = email;
+        this.AgentLatitude = latitude;
+        this.AgentLongitude = longitude;
+    }
+}
+
+/**
  * Sends post requests to "https://fos-tracker-278709.an.r.appspot.com/update_agent_location"
  * for updating latitude and longitude of agent in database whose coordinates are
  * given in the CoordinatesFile.
  */
-class ThreadforSendingLocationofAgent extends Thread{
-
+class ThreadForSendingLocationOfAgent extends Thread{
+    private static final int UPDATE_TIME_IN_MILLISECONDS = 2000;
     String CoordinatesFile;
-    ThreadforSendingLocationofAgent(String CoordinatesFile){
+    ThreadForSendingLocationOfAgent(String CoordinatesFile){
         this.CoordinatesFile = CoordinatesFile;
     }
 
     @Override
     public void run(){
         try{
-            String email, latitude, longitude;
+            String email;
+            double latitude, longitude;
             BufferedReader br = new BufferedReader(new FileReader(CoordinatesFile));
             email = br.readLine();
             String latitude_longitude = br.readLine();
             while (latitude_longitude!=null){
 
                 String[] latitude_longitude_tuple = latitude_longitude.split(" ");
-                latitude = latitude_longitude_tuple[0];
-                longitude = latitude_longitude_tuple[1];
-                String jsonRequest = "{\"email\":\""+email+"\",\"latitude\":"+latitude+",\"longitude\":"+longitude+"}";
+                latitude = Double.parseDouble(latitude_longitude_tuple[0]);
+                longitude = Double.parseDouble(latitude_longitude_tuple[1]);
+                AgentLocationInfo agent = new AgentLocationInfo(email, latitude, longitude);
+                String jsonRequest = agent.toJson();
                 byte[] out = jsonRequest.getBytes(StandardCharsets.UTF_8);
                 int length = out.length;
 
                 URL url = new URL("https://fos-tracker-278709.an.r.appspot.com/update_agent_location");
                 URLConnection con = url.openConnection();
                 HttpURLConnection http = (HttpURLConnection)con;
-                http.setRequestMethod("POST"); // PUT is another valid option
+                http.setRequestMethod("POST");
                 http.setDoOutput(true);
 
                 http.setFixedLengthStreamingMode(length);
@@ -49,7 +76,7 @@ class ThreadforSendingLocationofAgent extends Thread{
 
                 latitude_longitude = br.readLine();
                 // Coordinates are updated every 2 seconds
-                sleep(2000);
+                sleep(UPDATE_TIME_IN_MILLISECONDS);
             }
         }
         catch(Exception e){
@@ -70,7 +97,7 @@ public class AgentLocationSimulator{
      */
     public static void main(String[] args) {
         for (int i = 0; i < args.length; i++){
-            ThreadforSendingLocationofAgent agentTransistion = new ThreadforSendingLocationofAgent(args[i]);
+            ThreadForSendingLocationOfAgent agentTransistion = new ThreadForSendingLocationOfAgent(args[i]);
             agentTransistion.start();
         }
     }
