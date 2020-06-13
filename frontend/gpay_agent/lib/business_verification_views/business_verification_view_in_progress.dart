7a810f939deb46e73bc487a09c738a8e2c88b1d3
vahinim <vahinim@google.com>
2020-06-14 01:11:31 +0530

    present update

--- frontend/gpay_agent/lib/business_verification_views/business_verification_view_in_progress.dart
+++ frontend/gpay_agent/lib/business_verification_views/business_verification_view_in_progress.dart
@@ -8,7 +8,42 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+enum iconType{
+  face,
+  store,
+  phone
+}
+
+enum verificationType{
+  finish,
+  revisit,
+  store_does_not_exist,
+  cancel
+}
+
+enum selectCategory{
+  merchant_details
+}
+
+enum headerName{
+  merchant_details,
+  business_details,
+  business_phone
+}
+
 class _MyHomePageState extends State<MyHomePage> {
+
+  bool isSelected = false;
+  bool isNameCorrect = false;
+  bool isAddressCorrect = false;
+  bool isPhoneCorrect = false;
+  double headerHeight = 50;
+  double headerTextSize = 14;
+  double buttonHeight = 50;
+  double buttonTextSize = 14;
+  double itemHeight = 100;
+  double itemTextSize = 12;
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
@@ -23,28 +58,28 @@ class _MyHomePageState extends State<MyHomePage> {
                   (BuildContext context, int index) {
                 /// To convert this infinite list to a list with "n" no of items,
                 /// uncomment the following line:
-                if (index > 10) return null;
+                if (index > 9) return null;
                 switch (index) {
                   case 0:
-                    return _merchantDetailsHeaderItem(height);
+                    return _buildHeaderItem(headerHeight, headerTextSize, iconType.face, headerName.merchant_details);
                   case 1:
-                    return _merchantNameItem(height, width);
+                    return _buildListItem(itemHeight, itemTextSize, headerName.merchant_details);
                   case 2:
-                    return _businessDetailsHeaderItem(height);
+                    return _buildHeaderItem(headerHeight, headerTextSize, iconType.store, headerName.business_details);
                   case 3:
-                    return listItem(Colors.brown, "Sliver List item: $index");
+                    return _buildListItem(itemHeight, itemTextSize, headerName.business_details);
                   case 4:
-                    return _businessPhoneItem(height);
+                    return _buildHeaderItem(headerHeight, headerTextSize, iconType.phone, headerName.business_phone);
                   case 5:
-                    return listItem(Colors.brown, "Sliver List item: $index");
+                    return _buildListItem(itemHeight, itemTextSize, headerName.business_phone);
+                  case 6:
+                    return _buildButtonItem(buttonHeight, buttonTextSize, verificationType.finish);
                   case 7:
-                    return _finishVerificationItem(height);
+                    return _buildButtonItem(buttonHeight, buttonTextSize, verificationType.revisit);
                   case 8:
-                    return _needAnotherVisitItem(height);
+                    return _buildButtonItem(buttonHeight, buttonTextSize, verificationType.store_does_not_exist);
                   case 9:
-                    return _storeDoesNotExistItem(height);
-                  case 10:
-                    return _cancelItem(height);
+                    return _buildButtonItem(buttonHeight, buttonTextSize, verificationType.cancel);
                   default:
                     return listItem(Colors.green, "Sliver List item: $index");
                 }
@@ -67,7 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
       snap: false,
       flexibleSpace: const FlexibleSpaceBar(
         title: Text(
-            'Medidoddi Vahini'
+          'Medidoddi Vahini',
         ),
         centerTitle: true,
       ),
@@ -124,24 +159,24 @@ class _MyHomePageState extends State<MyHomePage> {
     ),
   );
 
-  Widget _merchantDetailsHeaderItem(double height) {
+  Widget _buildHeaderItem(double height, double textSize, iconType icon, headerName name){
     return Padding(
       padding: EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 6.0),
       child: Container(
-        height: 70,
+        height: height,
         child: Row(
           children: <Widget>[
             FittedBox(
-                fit: BoxFit.contain, // otherwise the logo will be tiny
-                child: Icon(
-                  Icons.face,
-                )),
+              fit: BoxFit.contain, // otherwise the logo will be tiny
+              child: _selectIcon(icon),
+            ),
             Padding(
               padding: EdgeInsets.all(16.0),
-              child: Text('MERCHANT DETAILS',
+              child: Text(
+                  _getHeaderText(name),
                   textAlign: TextAlign.left,
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: textSize,
                   )),
             ),
           ],
@@ -150,72 +185,37 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _businessDetailsHeaderItem(double height) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 6.0),
-      child: Container(
-        height: 70,
-        child: Row(
-          children: <Widget>[
-            FittedBox(
-                fit: BoxFit.contain, // otherwise the logo will be tiny
-                child: Icon(
-                  Icons.store,
-                )),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text('BUSINESS DETAILS',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 16,
-                  )),
-            ),
-          ],
-        ),
-      ),
-    );
+  Widget _selectIcon(iconType icon){
+    switch(icon){
+      case iconType.face:
+        return Icon(
+          Icons.face,
+        );
+      case iconType.phone:
+        return Icon(
+          Icons.phone,
+        );
+      case iconType.store:
+        return Icon(
+          Icons.store,
+        );
+    }
   }
 
-  Widget _businessPhoneItem(double height) {
+  Widget _buildButtonItem(double height, double textSize,verificationType type) {
     return Padding(
       padding: EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 6.0),
       child: Container(
-        height: 70,
-        child: Row(
-          children: <Widget>[
-            FittedBox(
-                fit: BoxFit.contain, // otherwise the logo will be tiny
-                child: Icon(
-                  Icons.phone,
-                )),
-            Padding(
-              padding: EdgeInsets.all(16.0),
-              child: Text('BUSINESS PHONE',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                    fontSize: 16,
-                  )),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _needAnotherVisitItem(double height) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 6.0),
-      child: Container(
-        height: 50,
+        height: height,
         child: OutlineButton(
           shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(12.0)),
           highlightedBorderColor: Colors.black,
           onPressed: () { },
           child: Text(
-            'Needs another visit',
+            _getVerificationTypeText(type),
             style: TextStyle(
               color: Colors.blue,
-              fontSize: 20,
+              fontSize: textSize,
             ),
           ),
           borderSide: BorderSide(
@@ -226,85 +226,35 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _finishVerificationItem(double height) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 6.0),
-      child: Container(
-        height: 50,
-        child: OutlineButton(
-          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(12.0)),
-          highlightedBorderColor: Colors.black,
-          onPressed: () { },
-          child: Text(
-            'Finish Verification',
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 20,
-            ),
-          ),
-          borderSide: BorderSide(
-            color: Colors.black,
-          ),
-        ),
-      ),
-    );
+  String _getVerificationTypeText(verificationType type){
+    switch(type){
+      case verificationType.finish:
+        return 'Finish Verification';
+      case verificationType.revisit:
+        return 'Needs another visit';
+      case verificationType.store_does_not_exist:
+        return 'Store does not exist';
+      case verificationType.cancel:
+        return 'Cancel';
+    }
   }
 
-  Widget _storeDoesNotExistItem(double height) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 6.0),
-      child: Container(
-        height: 50,
-        child: OutlineButton(
-          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(12.0)),
-          highlightedBorderColor: Colors.black,
-          splashColor: Colors.blueAccent,
-          onPressed: () { },
-          child: Text(
-            'Store does not exist',
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 20,
-            ),
-          ),
-          borderSide: BorderSide(
-            color: Colors.black,
-          ),
-        ),
-      ),
-    );
+  String _getHeaderText(headerName name){
+    switch(name){
+      case headerName.merchant_details:
+        return 'MERCHANT DETAILS';
+      case headerName.business_details:
+        return 'BUSINESS DETAILS';
+      case headerName.business_phone:
+        return 'BUSINESS PHONE';
+    }
   }
 
-  Widget _cancelItem(double height) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 6.0),
-      child: Container(
-        height: 50,
-        child: OutlineButton(
-          shape: new RoundedRectangleBorder(borderRadius: new BorderRadius.circular(12.0)),
-          highlightedBorderColor: Colors.black,
-          onPressed: () { },
-          child: Text(
-            'Cancel',
-            style: TextStyle(
-              color: Colors.blue,
-              fontSize: 20,
-            ),
-          ),
-          borderSide: BorderSide(
-            color: Colors.black,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _merchantNameItem(double height, double width) {
-    double itemHeight = height * 0.1;
+  Widget _buildListItem(double itemHeight, double textSize, headerName name) {
     return Padding(
         padding: EdgeInsets.fromLTRB(12.0, 6.0, 12.0, 6.0),
         child: Container(
-          height: 100,
+          height: itemHeight,
           child: Row(
             children: <Widget>[
               Expanded(
@@ -316,57 +266,140 @@ class _MyHomePageState extends State<MyHomePage> {
                           child: Align(
                               alignment: Alignment.bottomCenter,
                               child: Text(
-                                'Merchant Name',
+                                _getFieldName(name),
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
                               )
                           )
                       ),
                       Expanded(
                           flex: 1,
                           child: Text(
-                            globals.merchantName,
+                            _getFieldValue(name),
                           )
                       )
                     ],
                   )
               ),
               Expanded(
-                  flex: 1,
-                  child: FittedBox(
-                      child: RawMaterialButton(
-                        onPressed: () {},
-                        elevation: 0,
-                        fillColor: Colors.white,
-                        child: Icon(
-                          Icons.pause,
-                        ),
-                        shape: CircleBorder(),
-                      )
-                  )
-              ),
-              Expanded(
-                  flex: 1,
-                  child: FittedBox(
-                      child: RawMaterialButton(
-                        onPressed: () {},
-                        elevation: 0,
-                        fillColor: Colors.blue,
-                        /*child: Icon(
-                          Icons.pause,
-                        ),*/
-                        shape: CircleBorder(),
-                      )
-                  )
+                  flex: 2,
+                  child: _buildIcons(name)
               )
             ],
           ),
           decoration: BoxDecoration(
             border: Border.all(
-              color: Colors.black,
+              color: Colors.grey,
               width: 2,
             ),
             borderRadius: BorderRadius.circular(12),
           ),
         )
+    );
+  }
+
+  String _getFieldName(headerName name){
+    switch(name){
+      case headerName.merchant_details:
+        return 'Merchant Name';
+      case headerName.business_details:
+        return 'Address';
+      case headerName.business_phone:
+        return 'Phone Number';
+    }
+  }
+
+  String _getFieldValue(headerName name){
+    switch(name){
+      case headerName.merchant_details:
+        return globals.merchantName;
+      case headerName.business_details:
+        return globals.address;
+      case headerName.business_phone:
+        return globals.phone;
+    }
+  }
+
+  bool _getVariableValue(headerName name){
+    switch(name){
+      case headerName.merchant_details:
+        return isNameCorrect;
+      case headerName.business_details:
+        return isAddressCorrect;
+      case headerName.business_phone:
+        return isPhoneCorrect;
+    }
+  }
+
+  void _setVariableValue(headerName name, bool value){
+    switch(name){
+      case headerName.merchant_details:
+        isNameCorrect = value;
+        break;
+      case headerName.business_details:
+        isAddressCorrect = value;
+        break;
+      case headerName.business_phone:
+        isPhoneCorrect = value;
+        break;
+    }
+  }
+
+  Widget _buildIcons(headerName name){
+    return Row(
+      children: <Widget>[
+        Expanded(
+          flex: 1,
+          child: InkWell(
+            splashColor: Colors.white,
+            onTap: (){
+              _setVariableValue(name,false);
+              setState(() {});
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 2,
+                ),
+                color: !_getVariableValue(name) ? Colors.red[100] : Colors.white,
+                shape: BoxShape.circle,
+              ),
+              child: Icon(
+                Icons.close,
+                color: !_getVariableValue(name) ? Colors.red : Colors.blue,
+              ),
+            ),
+          ),
+        ),
+        Expanded(
+          flex: 1,
+          child:InkWell(
+            splashColor: Colors.white,
+            onTap: (){
+              _setVariableValue(name,true);
+              setState(() {});
+            },
+            child: Container(
+              padding: EdgeInsets.symmetric(vertical: 12.0, horizontal: 12.0),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.grey,
+                  width: 2,
+                ),
+                color: _getVariableValue(name) ? Colors.green[100] : Colors.white,
+                shape: BoxShape.circle,
+              ),
+              child:Icon(
+                Icons.done,
+                color: _getVariableValue(name) ? Colors.green : Colors.blue,
+              ),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
--- frontend/gpay_agent/lib/business_verification_views/globals.dart
+++ frontend/gpay_agent/lib/business_verification_views/globals.dart
@@ -1,3 +1,5 @@
 library gpay_agent.globals;
 
 final String merchantName = 'Medidoddi Vahini';
+final String address = 'H-no:15-108/1/k, Raghavendra colony, Nagarkurnool, Telangana';
+final String phone = '9381143249';
