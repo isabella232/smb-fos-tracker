a6d193027138d08680bc962ea00fea5d7d71df3d
pragyasethi <pragyasethi@google.com>
2020-06-14 14:27:07 +0530

    Creates agent object by fetching data from servlet

---
+++ frontend/gpay_agent/lib/agent_datamodels/coordinates.dart
@@ -0,0 +1,22 @@
+class MapCoordinates{
+  double latitude;
+  double longitude;
+
+  MapCoordinates(
+      this.latitude,
+      this.longitude
+      );
+
+  Map<String, dynamic> toJson() =>
+      {
+        'latitude': latitude,
+        'longitude': longitude,
+      };
+
+  factory MapCoordinates.fromJson(Map<String, dynamic> json) {
+    return MapCoordinates(
+      json['latitude'] as double,
+      json['longitude'] as double,
+    );
+  }
+}
