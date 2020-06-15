82bea5639fb40b814bdfa5077577796b553bd966
vahinim <vahinim@google.com>
2020-06-15 09:52:31 +0530

    Add http post method to send data to backend

---
+++ frontend/gpay_agent/lib/business_verification_data/verification.dart
@@ -0,0 +1,43 @@
+
+import 'dart:convert';
+import 'package:agent_app/business_verification_data/globals.dart' as globals;
+
+import 'package:agent_app/business_verification_data/Coordinates.dart';
+
+class Verification {
+  String agentEmail;
+  String storePhone;
+  Coordinates verificationCoordinates;
+  globals.verificationStatus status;
+
+  Verification(
+      {this.agentEmail,
+        this.storePhone,
+        this.verificationCoordinates,
+        this.status});
+
+  String convertToJsonString(){
+    return jsonEncode( <String ,dynamic>{
+      "agentEmail":agentEmail,
+      "storePhone":storePhone,
+      "verificationCoordinates":{
+        "latitude":verificationCoordinates.latitude,
+        "longitude":verificationCoordinates.longitude
+      },
+      "verificationStatus":_getStatus(status)
+    });
+  }
+
+  String _getStatus(globals.verificationStatus status){
+    switch(status){
+      case globals.verificationStatus.success:
+        return "green";
+      case globals.verificationStatus.failure:
+        return "red";
+      case globals.verificationStatus.needs_revisit:
+        return "yellow";
+      case globals.verificationStatus.not_verified:
+        return "grey";
+    }
+  }
+}
