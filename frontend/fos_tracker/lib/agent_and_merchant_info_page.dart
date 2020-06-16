49a4c7cae262df2f81f443a270ab28f8093ec89f
agarwalva <agarwalva@google.com>
2020-06-16 22:41:04 +0530

    Removed implementation, only the boilerplate

--- frontend/fos_tracker/lib/agent_and_merchant_info_page.dart
+++ frontend/fos_tracker/lib/agent_and_merchant_info_page.dart
@@ -1,13 +1,8 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:core';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:http/http.dart' as http;
-import 'package:sliding_up_panel/sliding_up_panel.dart';
-
-import 'globals.dart' as globals;
 
 /*
   This class contains the UI for the view that is generated on clicking on an agent in the main map.
@@ -22,7 +17,6 @@ class AgentPage extends StatefulWidget {
 }
 
 class _AgentPageState extends State<AgentPage> {
-  //TODO: fetch agent verifications
   Completer<GoogleMapController> controller;
   String agentFirstName;
   String agentName;
@@ -41,90 +35,11 @@ class _AgentPageState extends State<AgentPage> {
     super.initState();
   }
 
-  Future<void> getAgentInfo() async {
-    var result = await http.post(
-        "https://fos-tracker-278709.an.r.appspot.com/agent/email",
-        body: jsonEncode(<String, String>{"agentEmail": widget.agentEmail}));
-    if (result.statusCode != 200) {
-      setState(() {});
-    } else {
-      try {
-        LineSplitter lineSplitter = new LineSplitter();
-        List<String> lines = lineSplitter.convert(result.body);
-        String json = lines[0];
-        var jsonDecoded = jsonDecode(json);
-        setState(() {
-          agentFirstName = jsonDecoded['name']['firstName'];
-          agentName = jsonDecoded['name']['firstName'] +
-              ' ' +
-              jsonDecoded['name']['middleName'] +
-              ' ' +
-              jsonDecoded['name']['lastName'];
-          agentPhone = jsonDecoded['phone'];
-          agentCreationTime = jsonDecoded['agentCreationDateTime'];
-        });
-      } catch (e) {
-        print(e + "error parsing agent info");
-      }
-    }
-  }
+  Future<void> getAgentInfo() async {}
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-        title: Text(widget.agentEmail),
-      ),
-      body: SlidingUpPanel(
-        backdropEnabled: true,
-        panel: Center(
-          child: agentFirstName == null
-              ? Center(
-                  child: CircularProgressIndicator(),
-                )
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text("Name: " + agentName),
-                    Text("Phone: " + agentPhone),
-                    Text("Email: " + widget.agentEmail),
-                    Text("Time of creation: " + agentCreationTime)
-                  ],
-                ),
-        ),
-        body: GoogleMap(
-          mapType: MapType.normal,
-          initialCameraPosition: CameraPosition(
-            target: globals.startPosition,
-            zoom: 14.4746,
-          ),
-          onMapCreated: _onMapCreated,
-          zoomGesturesEnabled: true,
-          myLocationEnabled: true,
-          compassEnabled: true,
-          onCameraMove: (position) {},
-        ),
-        collapsed: Center(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(Icons.keyboard_arrow_up),
-              (agentFirstName == null || agentFirstName.isEmpty)
-                  ? Text(" Swipe up to view agent info")
-                  : Text(" Swipe up to view " + agentFirstName + "'s info"),
-              Icon(Icons.keyboard_arrow_up)
-            ],
-          ),
-        ),
-        minHeight: MediaQuery.of(context).size.height * 0.06,
-      ),
-    );
+    return Scaffold();
   }
 }
 
@@ -153,81 +68,10 @@ class _MerchantPageState extends State<MerchantPage> {
     super.initState();
   }
 
-  Future<void> getMerchantInfo() async {
-    var result = await http.post(
-        "https://fos-tracker-278709.an.r.appspot.com/store/phone",
-        body: jsonEncode(<String, String>{"storePhone": widget.storePhone}));
-    if (result.statusCode != 200) {
-      setState(() {});
-    } else {
-      try {
-        LineSplitter lineSplitter = new LineSplitter();
-        List<String> lines = lineSplitter.convert(result.body);
-        String json = lines[0];
-        var jsonDecoded = jsonDecode(json);
-        setState(() {
-          storeName = jsonDecoded['storeName'];
-          ownerName = jsonDecoded['ownerName']['firstName'] +
-              ' ' +
-              jsonDecoded['ownerName']['middleName'] +
-              ' ' +
-              jsonDecoded['ownerName']['lastName'];
-          storeCreationTime = jsonDecoded['creationDateTime'];
-          address = jsonDecoded['storeAddress']['street'] +
-              "\n" +
-              jsonDecoded['storeAddress']['area'] +
-              "\n" +
-              jsonDecoded['storeAddress']['city'] +
-              "\n" +
-              jsonDecoded['storeAddress']['state'] +
-              "\n" +
-              jsonDecoded['storeAddress']['pincode'] +
-              "\n" +
-              jsonDecoded['storeAddress']['country'];
-        });
-      } catch (e) {
-        print(e + "error parsing merchant info");
-      }
-    }
-  }
+  Future<void> getMerchantInfo() async {}
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.pop(context);
-            },
-          ),
-          title: Text(widget.storePhone),
-        ),
-        body: storeName == null
-            ? Center(child: CircularProgressIndicator())
-            : Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      storeName,
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    Text(
-                      "Owner: " + ownerName,
-                    ),
-                    Text(
-                      "Phone: " + widget.storePhone,
-                    ),
-                    Text(
-                      "Address: " + address,
-                    ),
-                    Text(
-                      "Creation time: " + storeCreationTime,
-                    ),
-                  ],
-                ),
-              ));
+    return Scaffold();
   }
 }
